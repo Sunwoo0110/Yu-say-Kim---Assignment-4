fe288e4cf14ab64d1aa2e3527891887d70440500
jihooni0914 <dbwlgns8314@gmail.com>
2023-01-23 04:35:35 +0900

    change jihoon's workflow and src/PR_comment.js

--- src/PR_comment.js
+++ src/PR_comment.js
@@ -1,11 +1,5 @@
-const sender = process.argv[2]
+const sender = "my good member"
 const review_comment = "LGTM!"
 
-console.log("sender : " + sender)
-
-if (sender === "Sunwoo0110" || sender === "jihooni0914") {
-    console.log("Hey " + sender + ", " + review_comment)
-}
-else {
-    console.log("Please wait for review and approve")
-}
+console.log("Hey " + sender + ", " + review_comment)
+console.log("This PR will be auto-merged soon. Wait for seconds!")
